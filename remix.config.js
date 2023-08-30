86dd6547a7a9e100cf2796784d64d43153b24206
Vishwajeetsinh Rathod <rathodvishwajeet64@gmail.com>
2023-08-30 20:51:29 +0530

    initial commit

---
+++ remix.config.js
@@ -0,0 +1,25 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+const {
+  createRoutesFromFolders,
+} = require("@remix-run/v1-route-convention");
+
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+  serverModuleFormat: "cjs",
+  future: {
+    v2_dev: true,
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes);
+  }
+};
